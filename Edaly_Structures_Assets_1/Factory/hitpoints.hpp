ddc613a84a03c725d2ef49915f6158642be34416
Sarah Kieffert <moon-gaming@hotmail.com>
2025-05-12 07:41:21 +0200

    Edaly_Structures

---
+++ Edaly_Structures_Assets_1/Factory/hitpoints.hpp
@@ -0,0 +1,212 @@
+                class Glass_1_hitpoint
+                {
+                    armor = 0.0005;
+                    material = -1;
+                    name = "Glass_1_hitpoint";
+                    visual = "Glass_1_hide";
+                    passThrough = 0;
+                    radius = 0.25;
+                    convexComponent = "Glass_1_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(1,1)
+                        BIG_GLASS_HITPOINT(1,2)
+                        BIG_GLASS_HITPOINT(1,3)
+                    };
+                };
+
+                class Glass_2_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_2_hitpoint";
+                    visual = "Glass_2_hide";
+                    convexComponent = "Glass_2_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(2,1)
+                        BIG_GLASS_HITPOINT(2,2)
+                        BIG_GLASS_HITPOINT(2,3)
+                    };
+                };
+
+                class Glass_3_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_3_hitpoint";
+                    visual = "Glass_3_hide";
+                    convexComponent = "Glass_3_hide";
+                    class DestructionEffects
+                    {
+                        DOOR_GLASS_HITPOINT(3,1)
+                    };
+                };
+
+                class Glass_4_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_4_hitpoint";
+                    visual = "Glass_4_hide";
+                    convexComponent = "Glass_4_hide";
+                    class DestructionEffects
+                    {
+                        DOOR_GLASS_HITPOINT(4,1)
+                    };
+                };
+
+                class Glass_5_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_5_hitpoint";
+                    visual = "Glass_5_hide";
+                    convexComponent = "Glass_5_hide";
+                    class DestructionEffects
+                    {
+                        DOOR_GLASS_HITPOINT(5,1)
+                    };
+                };
+
+                class Glass_6_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_6_hitpoint";
+                    visual = "Glass_6_hide";
+                    convexComponent = "Glass_6_hide";
+                    class DestructionEffects
+                    {
+                        DOOR_GLASS_HITPOINT(6,1)
+                    };
+                };
+
+                class Glass_7_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_7_hitpoint";
+                    visual = "Glass_7_hide";
+                    convexComponent = "Glass_7_hide";
+                    class DestructionEffects
+                    {
+                        DOOR_GLASS_HITPOINT(7,1)
+                    };
+                };
+
+                class Glass_8_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_8_hitpoint";
+                    visual = "Glass_8_hide";
+                    convexComponent = "Glass_8_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(8,1)
+                        BIG_GLASS_HITPOINT(8,2)
+                        BIG_GLASS_HITPOINT(8,3)
+                        BIG_GLASS_HITPOINT(8,4)
+                        BIG_GLASS_HITPOINT(8,5)
+                        BIG_GLASS_HITPOINT(8,6)
+                        BIG_GLASS_HITPOINT(8,7)
+                        BIG_GLASS_HITPOINT(8,8)
+                    };
+                };
+
+                class Glass_9_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_9_hitpoint";
+                    visual = "Glass_9_hide";
+                    convexComponent = "Glass_9_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(9,1)
+                        BIG_GLASS_HITPOINT(9,2)
+                        BIG_GLASS_HITPOINT(9,3)
+                        BIG_GLASS_HITPOINT(9,4)
+                        BIG_GLASS_HITPOINT(9,5)
+                        BIG_GLASS_HITPOINT(9,6)
+                        BIG_GLASS_HITPOINT(9,7)
+                        BIG_GLASS_HITPOINT(9,8)
+                    };
+                };
+
+                class Glass_10_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_10_hitpoint";
+                    visual = "Glass_10_hide";
+                    convexComponent = "Glass_10_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(10,1)
+                        BIG_GLASS_HITPOINT(10,2)
+                        BIG_GLASS_HITPOINT(10,3)
+                        BIG_GLASS_HITPOINT(10,4)
+                    };
+                };
+
+                class Glass_11_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_11_hitpoint";
+                    visual = "Glass_11_hide";
+                    convexComponent = "Glass_11_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(11,1)
+                        BIG_GLASS_HITPOINT(11,2)
+                        BIG_GLASS_HITPOINT(11,3)
+                    };
+                };
+
+                class Glass_12_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_12_hitpoint";
+                    visual = "Glass_12_hide";
+                    convexComponent = "Glass_12_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(12,1)
+                        BIG_GLASS_HITPOINT(12,2)
+                        BIG_GLASS_HITPOINT(12,3)
+                    };
+                };
+
+                class Glass_13_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_13_hitpoint";
+                    visual = "Glass_13_hide";
+                    convexComponent = "Glass_13_hide";
+                    class DestructionEffects
+                    {
+                        NORMAL_GLASS_HITPOINT(13,1)
+                    };
+                };
+
+                class Glass_14_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_14_hitpoint";
+                    visual = "Glass_14_hide";
+                    convexComponent = "Glass_14_hide";
+                    class DestructionEffects
+                    {
+                        NORMAL_GLASS_HITPOINT(14,1)
+                        NORMAL_GLASS_HITPOINT(14,2)
+                        NORMAL_GLASS_HITPOINT(14,3)
+                    };
+                };
+
+                class Glass_15_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_15_hitpoint";
+                    visual = "Glass_15_hide";
+                    convexComponent = "Glass_15_hide";
+                    class DestructionEffects
+                    {
+                        NORMAL_GLASS_HITPOINT(15,1)
+                        NORMAL_GLASS_HITPOINT(15,2)
+                        NORMAL_GLASS_HITPOINT(15,3)
+                    };
+                };
+
+                class Glass_16_hitpoint : Glass_1_hitpoint
+                {
+                    name = "Glass_16_hitpoint";
+                    visual = "Glass_16_hide";
+                    convexComponent = "Glass_16_hide";
+                    class DestructionEffects
+                    {
+                        BIG_GLASS_HITPOINT(16,1)
+                        BIG_GLASS_HITPOINT(16,2)
+                        BIG_GLASS_HITPOINT(16,3)
+                        BIG_GLASS_HITPOINT(16,4)
+                        BIG_GLASS_HITPOINT(16,5)
+                    };
+                };
---
+++ Edaly_Structures_Assets_10/Mill/hitpoints.hpp
@@ -0,0 +1,28 @@
+            class glass_1_hitpoint
+            {
+                armor = 0.0005;
+                material = -1;
+                name = "glass_1_hitpoint";
+                visual = "glass_1_hide";
+                passThrough = 0;
+                radius = 0.25;
+                convexComponent = "glass_1_hide";
+                class DestructionEffects
+                {
+                    NORMAL_GLASS_HITPOINT(1,1)
+                };
+            };
+            class glass_2_hitpoint
+            {
+                armor = 0.0005;
+                material = -1;
+                name = "glass_2_hitpoint";
+                visual = "glass_2_hide";
+                passThrough = 0;
+                radius = 0.25;
+                convexComponent = "glass_2_hide";
+                class DestructionEffects
+                {
+                    NORMAL_GLASS_HITPOINT(2,1)
+                };
+            };
