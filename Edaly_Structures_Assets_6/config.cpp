29a2d1562324c6dd46b9e73a81470298104829a1
Sarah Kieffert <moon-gaming@hotmail.com>
2025-05-12 07:33:42 +0200

    Edaly_Structures_Assets_6

---
+++ Edaly_Structures_Assets_6/config.cpp
@@ -0,0 +1,14 @@
+#include "\Edaly_Client\version.hpp"
+
+class CfgPatches
+{
+	class Edaly_Structures_Assets_6
+	{
+		author=$STR_EDALY_AUTHOR;
+		url=$STR_EDALY_WEBSITE;
+		requiredAddons[]={"Edaly_Client"};
+		requiredVersion=ARMA3_VERSION;
+		units[]={};
+		weapons[]={};
+	};
+};
